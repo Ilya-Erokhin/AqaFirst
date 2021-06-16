52066320494ee2de6ecc7d4f357c33535b80cf6a
Ilya Erokhin <illyaerohin@yandex.ru>
2021-06-16 20:18:29 +0300

    testng was added

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -46,4 +46,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldIfAmountEqualBoundary() {
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 }
